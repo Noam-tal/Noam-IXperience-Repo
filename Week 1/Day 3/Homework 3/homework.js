7e2ad02da95ea4d55c98f1051764923980c96fa6
Noam-tal <noamptal@gmail.com>
2023-06-01 15:29:08 +0200

    commit

--- Week 1/Day 3/Homework 3/homework.js
+++ Week 1/Day 3/Homework 3/homework.js
@@ -1,5 +1,16 @@
-const numberOne = 0
-const numberTwo = 1
-
-const array = [0,1]
+function fibonacci (){
+    let number1 = 0;
+    console.log(number1)
+    let number2 = 1
+    console.log(number2)
+    let counter=0
+    while (counter<8){
+        let sum = number1+number2;
+        console.log(sum)
+        number1=number2;
+        number2=sum;
+        counter++
+    }
+   
+}
 
