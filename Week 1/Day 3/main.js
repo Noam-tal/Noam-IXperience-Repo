2ca8324e1d326d812a8ae81866a9a1e0407e26d0
Noam-tal <noamptal@gmail.com>
2023-06-01 09:46:29 +0200

    commit

---
+++ Week 1/Day 3/main.js
@@ -0,0 +1,15 @@
+let example = "example"
+
+console.log(example.charAt(2))
+
+val=Math.E
+console.log(val)
+
+for(let i=1; i<6; i++){
+    console.log(`Sheep Number: ${i}`)
+}
+
+let a=0
+console.log(a)
+
+
