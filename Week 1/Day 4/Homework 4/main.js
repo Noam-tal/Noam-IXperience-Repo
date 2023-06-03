a26e4a99a897b6ef32547ca1a94b0fad96253faf
Noam-tal <noamptal@gmail.com>
2023-06-03 18:47:00 +0200

    Commit

--- Week 1/Day 4/Homework 4/main.js
+++ Week 1/Day 4/Homework 4/main.js
@@ -1,19 +1,55 @@
+const movies = [
+    {title: 'Harry Potter', explanation: 'This movie is about a dude with a stick...', hint: 'It\'s Magic'},
+    {title: 'Just Go With It', explanation: 'This movie is about people who go on holiday...', hint: 'Adam, Drew and Jennifer'},
+    {title: 'Never Back Down', explanation: 'This movie is about two guys with daddy issues who beat each other up...', hint: 'Kanye West - Stronger'},
+    {title: 'Spongebob Squarepants', explanation: 'This movie is about a rectangle...', hint: 'It\'s a cartoon'},
+    {title: '50 First Dates', explanation: 'This movie is about a chick, she has the worst memory...', hint: '50 times...'},
+    {title: 'Cars', explanation: 'In this movie, car go fast...', hint: 'Kachow'},
+    {title: 'Spiderman', explanation: 'In this movie this guy just does not pay his rent, no matter how many times the landlord asks...', hint: 'Peta-Paka'},
+    {title: 'The Wolf Of Wall Street', explanation: 'In this movie there\'s like illegal stuff, lots of money, and a blonde chick...', hint: 'HAWOOooooooooooo'},
+    {title: 'Inception', explanation: 'In this movie everyone is like sleeping all the time...', hint: 'Dreams...'},
+    {title: 'Peter Pan', explanation: 'In this movie some kids die and an angel escorts them to heaven...', hint: 'Always flying, cuz he neverlands'},
+    {title: 'The Lord Of The Rings', explanation: 'In this movie some small guys go for a walk...', hint: 'You will not vacate past this exact position'}
+]
+console.log(movies[0].title)
+ 
+const desc = document.getElementById("descriptions")
 const guess = document.getElementById("guess-form")
-
-
-function makeGuess(){
-if (guess.value == "Reservoir Dogs") {
-    console.log("You Got It!")
-}
-else {
-    console.log("Sorry, try again")
-   
-}
-guess.value = ""
-}
-
-
-
 const hint = document.getElementById("hint-des")
+const wrong = document.getElementById('wrong')
+
+for (let i=0; i<movies.length; i++){
+    
+    console.log(movies[i].title)
+
+    function makeGuess(){
+        if (guess.value == movies[i].title) {
+            console.log("You Got It!");
+            hint.textContent = ""
+        }
+        else {
+            console.log("Sorry, try again");
+            wrong.textContent=='Sorry, try again'
+            return
+           
+        }
+
+        guess.value = ""
+    }
+        }
+        
+        function showHint(){
+
+
+        }
+            
+
+
+
+
+
+
+
+
 
 
